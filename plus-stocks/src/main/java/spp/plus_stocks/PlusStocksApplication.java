459827b73127ec44a5de0ccd173184e8fc06d36b
palaciopedro <153301312+palaciopedro@users.noreply.github.com>
2026-03-18 10:33:44 -0300

    initialized project

---
+++ plus-stocks/src/main/java/spp/plus_stocks/PlusStocksApplication.java
@@ -0,0 +1,13 @@
+package spp.plus_stocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlusStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlusStocksApplication.class, args);
+	}
+
+}
